a72b1d42ba5108f16d94dcd9294ed791b021490c
Sharif Akram <rifkit@gmail.com>
2023-08-05 19:48:16 -0400

    add open_multiple_url.rb program

---
+++ open_multiple_urls.rb
@@ -0,0 +1,17 @@
+# A program that automates the opening of my study environment
+
+require 'launchy'
+
+urls = ['boot.dev',
+        'https://chat.openai.com/',
+        'https://stackoverflow.com/',
+        'youtube.com',
+        'github.com']
+
+urls.each do |url|
+  begin
+    Launchy.open(url)
+  rescue StandardError => e
+    puts "Error opening the URL: #{e.message}"
+  end
+end
